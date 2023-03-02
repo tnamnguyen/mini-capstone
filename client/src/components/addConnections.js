e94d4c8b098cff63901edceb52ca0243eb940fee
eyadsw <76524254+eyadsw@users.noreply.github.com>
2023-03-02 17:20:07 -0500

    eyad

--- client/src/components/addConnections.js
+++ client/src/components/addConnections.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {  useEffect  } from 'react';
 import axios from "axios";
 import NavBar from './navBar';
 import '../Styles/sign-up.scss';
 
 function AddConnections(){
-    const [username,setName] = useState('');
+    const [users, displayUser] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    
+
+    // printing the users
+    useEffect(() => {
+        // Fetch all users from the backend API when the component mounts
+        axios.get(SERVER_URL+ '/connections')
+          .then(response => {
+              displayUser(response.data);
+              console.log(users, 'user data');
+          })
+          .catch(error => {
+            console.error('Error fetching users:', error);
+          });
+      }, []);
 
     const SERVER_URL = "http://localhost:3001"
     // const SERVER_URL = "https://jobilee-server.vercel.app"
@@ -21,10 +36,50 @@ function AddConnections(){
             <form >
                 <label>
                 Username
-                <input required type="text" value={username} onChange={(event) => setName(event.target.value)} />
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(event)=> {setInputValue(event.target.value); }}
+                />
+                
                 </label>
                 
-                <button type="submit">Submit</button>
+                
+
+                {/*  printing the users in table */}
+                <div className="container">
+            <table className="job-table">
+            <thead>
+                <tr>
+                <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            {users.filter((user) => {if (inputValue === ''){
+                return user} 
+                else if(typeof user.name === 'string' && inputValue.toLowerCase().includes(user.name)){
+
+                    return user
+                }})
+                .map(user => (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <button id='addToConnections' link=""> 
+                        Add
+                    </button>
+                </tr>
+                
+                    
+                
+                ))}
+                
+
+                
+            </tbody>
+            </table>
+            
+            </div>
                
             </form>
 
