b24d26ee7a511668c5fe0a2b7d858d7326adb904
eyadsw <76524254+eyadsw@users.noreply.github.com>
2023-04-13 16:51:34 -0400

    User can now see his sent connections and un-send

--- client/src/components/sentConnections.js
+++ client/src/components/sentConnections.js
@@ -5,47 +5,72 @@ import axios from "axios";
 import NavBar from "./navBar";
 import "../Styles/connections.scss";
 
-function Connections() {
+function SentConnections() {
   const [connections, setConnections] = useState([]);
   const accessToken = localStorage.getItem("token");
   const [user1, setUser1] = useState("");
   const [refetch, setRefetch] = useState(0);
   const [showPopUp, setShowPopUp] = useState("");
-
+  
   const SERVER_URL = process.env.REACT_APP_SERVER_URL
-
 
   // assigning user 1 as the logged-in user
   const isTokenAvailable = localStorage.getItem("token") != null;
   useEffect(() => {
     if (isTokenAvailable) {
       axios.post(SERVER_URL + "/home", { accessToken }).then((response) => {
-        setUser1(response.data.user);
+        setUser1(response.data?.user);
         console.log("the logged in user is " + user1);
       });
     }
   }, []);
-
   useEffect(() => {
     if (user1?.id) {
       axios
-        .get(SERVER_URL + `/connections/${user1.id}`)
+        .get(SERVER_URL + `/getSentConnection/${user1?.id}`)
         .then((response) => {
-          setConnections(response?.data?.data);
+          setConnections(response.data.data);
           console.log("Mounted");
-          console.log(response);
+          console.log(user1.name);
           console.log(connections, "user connections");
         })
         .catch((error) => {
           console.error("Error fetching Connections in front-end:", error);
         });
     }
-  }, [user1, refetch]);
-  console.log(user1.id);
+  }, [user1?.id, user1.name, refetch]);
+
 
   const handleRemove = async (id) => {
     await axios
       .delete(SERVER_URL + `/connections/${id}`)
+      .then((response) => {
+        console.log(response);
+        //If backend returns an error
+        if (response.data.status) {
+          setShowPopUp("connection Denied");
+          setRefetch(refetch + 1);
+          setTimeout(() => {
+            setShowPopUp("");
+          }, 3000);
+        } else {
+          setTimeout(() => {
+            window.location.href = "http://localhost:3000/";
+          }, 4000);
+        }
+      })
+      .catch((error) => {});
+  };
+
+  const handleUpdate = async (id, status) => {
+    // Do something with the form data, such as send it to a server
+
+    const data = {
+      status: status,
+    };
+    console.log(data);
+    await axios
+      .patch(SERVER_URL + `/connections/${id}`, data)
       .then((response) => {
         console.log(response);
         //If backend returns an error
@@ -68,16 +93,15 @@ function Connections() {
     <>
       <NavBar></NavBar>
 
-      <h1 id="job_title">Your connections</h1>
+      <h1 id="job_title">Connection Requests</h1>
       <br />
-      <div className="container">
-        <table className="connection-table" data-testid="connectionsTest">
+      <div className="container" data-testid="pendingConnectionsTest">
+        <table className="job-table">
           <thead>
             <tr>
               <th>Name</th>
               <th>Status</th>
-              <th>Send Message</th>
-              <th>Remove Connection</th>
+              <th>Cancel Request</th>
             </tr>
           </thead>
           <tbody>
@@ -85,21 +109,16 @@ function Connections() {
               <tr key={connection._id}>
                 <td>
                   {connection.user2Name === user1?.name
-                    ? connection.user1Name
-                    : connection.user2Name}
+                    ? connection.user2Name
+                    : connection.user1Name}
                 </td>
                 <td>{connection.status}</td>
-                <td className="message_td">
-                  <button onClick="" className="button-14">
-                    Message
-                  </button>
-                </td>
                 <td>
                   <button
                     onClick={() => handleRemove(connection?._id)}
                     className="button-14"
                   >
-                    Delete
+                    Un-send
                   </button>
                 </td>
               </tr>
@@ -108,14 +127,8 @@ function Connections() {
         </table>
       </div>
       <div>
-        <Link to="/addConnections" className="myButton">
-          add a connection
-        </Link>
-        <Link to="/pendingConnections" className="myButton">
-          Pending Connections
-        </Link>
-        <Link to="/SentConnections" className="myButton">
-          Your Sent Connections
+        <Link to="/Connections" className="myButton">
+          Your Connections
         </Link>
       </div>
       {showPopUp && <div className="success-message popup">{showPopUp}</div>}
@@ -123,4 +136,4 @@ function Connections() {
   );
 }
 
-export default Connections;
+export default SentConnections;
