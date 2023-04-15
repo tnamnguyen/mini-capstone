ee2a926cee0d36caf0799e69106314c137044150
tnamnguyen <tnam.ng97@gmail.com>
2023-04-15 18:21:11 -0400

    adding mocha and chai for SeleniumTests

--- client/SeleniumTests/loadHomePage.test.js
+++ client/SeleniumTests/loadHomePage.test.js
@@ -1,34 +1,25 @@
 // Loading WebDriver test module
 const {By, Key, Builder} = require("selenium-webdriver");
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
 require("chromedriver");
 
-// Declare test function
-async function test_case(){
-    // Declare driver session as chrome
-    let driver = await new Builder().forBrowser("chrome").build();
+describe('loading home page', function() {
+    it('should have a home page rendered', async function() {
+        this.timeout(10000);    // Allow the timeout of the test to be at 10 seconds instead
+        let driver = await new Builder().forBrowser('chrome').build();
 
-    // Navigate to website
-    // await driver.get("https://mini-capstone.vercel.app");
-    await driver.get("http://localhost:3000");
+        // await driver.get("https://mini-capstone.vercel.app");
+        await driver.get('http://localhost:3000/');
+        await driver.sleep(3000); // timeout for 3 seconds
 
-    //Wait for page to render after 3 minutes
-    setTimeout(function() {
-        driver.findElement(By.id('header-title'))
-        .then(element => {
-            console.log('Test passed');
-        })
-        .catch(error => {
-            console.log('Test failed');
-        })
-        .finally(() => {
-                // Set to quit program after 5 seconds
-        setInterval(function(){
-            driver.quit();
-        }, 5000);
-        })
-    }, 3000)
+        // Asserts
+        let header = await driver.findElement(By.className('header-title'));
+        let classes = await header.getAttribute('class');
+        expect(classes).to.include('header-title');
 
-}
+        // Quit the program
+        await driver.quit();
+    })
+})
 
-// Calling test case
-test_case();
